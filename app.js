942afb52ee17b4536b905a103c99c54b1e38ce9e
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-09 03:16:40 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const loadData = () =>{
+    const searchText = document.getElementById("serach-tex").value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`)
+    .then((res) => res.json())
+    .then((data) => displayData(data.meals));
+}
+
+const displayData = (data) => {
+    document.getElementById("total-item").innerText = data.length;
+    const itemsContainer = document.getElementById("items-container");
+    data.forEach(item=>{
+        const card = document.createElement("div");
+        card.classList.add("box");
+        card.innerHTML = `
+        <img class="box-img" src=${item.strMealThumb} alt="">
+        <h1>${item?.strMeal}</h1>
+        <p>${item.strInstructions.slice(0,80)}</p>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Item Details</button> 
+        `;
+        itemsContainer.appendChild(card);
+    })
+}
+
+
